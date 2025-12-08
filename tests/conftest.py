5bc387562074e0550b42d4fc12c905a6cf1d7b97
ClaudioFritzen <sdfritzen96@gmail.com>
2025-12-08 00:50:17 +0000

    implementação do alembic e criado tabelas

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,42 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import Session
 
 from fast_zero_async.app import app
+from fast_zero_async.models import table_registry
 
 
 @pytest.fixture
 def client():
     with TestClient(app) as c:
         yield c
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+    table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2025, 12, 7, 12, 0, 0)):
+
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+    yield time
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
---
+++ tests/test_db.py
@@ -0,0 +1,28 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fast_zero_async.models import User
+
+
+def test_create_user_adiciona_usuario_ao_database(session, mock_db_time):
+
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='johndoe',
+            email='johndoe@example.com',
+            password='securepassword',
+        )
+
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'johndoe'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'johndoe',
+        'email': 'johndoe@example.com',
+        'password': 'securepassword',
+        'created_at': time,
+    }
