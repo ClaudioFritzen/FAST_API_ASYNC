69f746b6e4c5ad4f6529df5c268e7446b03bbe51
ClaudioFritzen <sdfritzen96@gmail.com>
2026-03-10 17:31:06 +0000

    fix (rate_limiter) - deixamos explicito o limiter, para corrigir o erro do gitactions. - adicionamos alguns logs para ver o tempo de resposta em cada request do nosso banco de tests

--- fast_zero_async/middlewares/rate_limiter_middleware.py
+++ fast_zero_async/middlewares/rate_limiter_middleware.py
@@ -1,3 +1,4 @@
+import time
 from http import HTTPStatus
 
 from fastapi import Request
@@ -16,22 +17,31 @@ async def rate_limit_middleware(request: Request, call_next):
     print('🔥 TESTING =', TESTING)
     print('🔥 limmiter =', limiter)
 
+    # Inicio do rastreamento
+    start = time.perf_counter()
+
     if TESTING or limiter is None:
         print('🔥 TESTING=True → ignorando rate limit')
-        return await call_next(request)
+        # mudança importante: para pegar o tempo gasto
+        # em ambiente de teste, precisamos garantir que
+        #  o call_next seja chamado
+        response = await call_next(request)
+    else:
+        print('🔥 TESTING=False → aplicando rate limit')
+        user_id = request.headers.get('X-User-ID', 'anonymous')
+        print('🔥 user_id =', user_id)
+        allowed = await limiter.allow_request(user_id)
+        print('🔥 allowed =', allowed)
 
-    print('🔥 TESTING=False → aplicando rate limit')
-    user_id = request.headers.get('X-User-ID', 'anonymous')
-    print('🔥 user_id =', user_id)
-    allowed = await limiter.allow_request(user_id)
-    print('🔥 allowed =', allowed)
-
-    if not allowed:
-        print('🔥 BLOQUEADO POR RATE LIMIT')
-        return JSONResponse(
-            status_code=HTTPStatus.TOO_MANY_REQUESTS,
-            content={'detail': 'Limite de requisições excedido.'},
-        )
-    print('🔥 PASSOU PELO RATE LIMIT')
-    response = await call_next(request)
+        if not allowed:
+            print('🔥 BLOQUEADO POR RATE LIMIT')
+            return JSONResponse(
+                status_code=HTTPStatus.TOO_MANY_REQUESTS,
+                content={'detail': 'Limite de requisições excedido.'},
+            )
+        print('🔥 PASSOU PELO RATE LIMIT')
+        response = await call_next(request)
+    # Fim da medição
+    duration = time.perf_counter() - start
+    print(f'⏱️ TEMPO: Middleware - {request.url.path} ->: {duration:.4f} s')
     return response
