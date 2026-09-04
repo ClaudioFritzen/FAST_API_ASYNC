91a010a931d7a8a09757c323395785369f7b9a45
ClaudioFritzen <sdfritzen96@gmail.com>
2026-09-04 17:14:41 +0100

    modificamos para crial a URL

--- fast_zero_async/settings.py
+++ fast_zero_async/settings.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -6,7 +8,25 @@ class Settings(BaseSettings):
         env_file='.env', env_file_encoding='utf-8', extra='ignore'
     )
 
-    DATABASE_URL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PROD_DB: str
+    POSTGRES_HOST: str = 'db_prod'
+    POSTGRES_PORT: int = 5432
+
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f'postgresql+asyncpg://{self.POSTGRES_USER}:'
+            f'{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:'
+            f'{self.POSTGRES_PORT}/{self.POSTGRES_PROD_DB}'
+        )
+
+
+@lru_cache
+def get_settings():
+    return Settings()
