7f0f90f944c0d3a71ecfce2e974f9a07a397e7ff
ClaudioFritzen <sdfritzen96@gmail.com>
2026-09-04 18:07:39 +0100

    adicionando o testing e declarando o resto como nulo, pois nos tests não precisamos

--- fast_zero_async/settings.py
+++ fast_zero_async/settings.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -8,18 +8,23 @@ class Settings(BaseSettings):
         env_file='.env', env_file_encoding='utf-8', extra='ignore'
     )
 
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_PROD_DB: str
+    TESTING: bool = False
+
+    POSTGRES_USER: str | None = None
+    POSTGRES_PASSWORD: str | None = None
+    POSTGRES_PROD_DB: str   | None = None
     POSTGRES_HOST: str = 'db_prod'
     POSTGRES_PORT: int = 5432
 
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    SECRET_KEY: str | None = None
+    ALGORITHM: str  | None = None
+    ACCESS_TOKEN_EXPIRE_MINUTES: int | None = None
 
     @property
     def DATABASE_URL(self) -> str:
+        # CI / Testes
+        if self.TESTING:
+            return os.getenv('DATABASE_URL')
         return (
             f'postgresql+asyncpg://{self.POSTGRES_USER}:'
             f'{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:'
